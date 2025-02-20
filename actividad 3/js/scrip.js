2ed7dba1bd976da776b548252562c3a527aaee04
Antonio <alopcaz1009@g.educaand.es>
2025-02-20 22:24:30 +0100

    Add files via upload

---
+++ actividad 3/js/scrip.js
@@ -0,0 +1,9 @@
+function Pedirdatos() {
+    
+    let nombre = "" ;
+    let edad;
+         nombre = prompt("Cómo te llamas")    
+         edad = prompt("Cuántos años tienes")
+
+            alert("¡Hola" + nombre + "veo que tienes" + edad + "años.!")
+}
