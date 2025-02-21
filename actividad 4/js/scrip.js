6d6595fb42f4028fc4d44f6290e359fefe7e6d1a
Antonio <alopcaz1009@g.educaand.es>
2025-02-21 19:04:24 +0100

    Update scrip.js

--- actividad 4/js/scrip.js
+++ actividad 4/js/scrip.js
@@ -4,4 +4,5 @@ function Calcular() {
 
     a = parseInt(prompt ("Introduce el primer número:"));
     b = parseInt(prompt ("Introduce el segundo número:"));
+    alert (" El primer número es: " + a +"\n El segundo número es: "+ b + " \n La suma es:" +(a + b) +"\n La resta es" +(a - b) + "\n El producto es: " + (a * b) + "\n La división es: " + (a / b) + "\nEl resto es: " +(a % b)+ "");
   }
