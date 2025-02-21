8db96e0811a60dc5e3367917f1020c58d8bd216d
Antonio <alopcaz1009@g.educaand.es>
2025-02-21 18:56:56 +0100

    Update scrip.js

--- actividad 4/js/scrip.js
+++ actividad 4/js/scrip.js
@@ -1,6 +1,7 @@
-function Calcular() {
-    
+function Calcular() { 
     let a;
     let b;
 
+    a = parseInt(prompt ("Introduce el primer número:"));
+    b = parseInt(prompt ("Introduce el segundo número:"));
   }
